65e105acdd790bfb66de18f7507ccacb0282e76d
Harish <126079480+Harish-2608@users.noreply.github.com>
2023-07-18 20:17:21 +0530

    Add files via upload

---
+++ splashscreen/splashscreen.java
@@ -0,0 +1,26 @@
+package com.info.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(splashscreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
